7667af240921ed891cb730163ac5fb245c62ea05
Sylvia Chapman Kent <sylvia.kent.2.1@hotmail.com>
2025-03-10 16:09:48 +0000

    started assignment 4

---
+++ assignments/assignment04-github.py
@@ -0,0 +1,7 @@
+# assignment 4 - github
+# author: Sylvia Chapman Kent
+# reads a file from a repository, replaces all instances of the word "Andrew" with "Sylvia", then commits and pushes these changes
+
+import requests
+import json
+from config import key as cfg
