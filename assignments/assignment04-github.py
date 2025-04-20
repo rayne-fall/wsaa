f5fa4725b36089e16bee0088b47c01202ce2e2a4
Sylvia Chapman Kent <124678872+rayne-fall@users.noreply.github.com>
2025-04-20 18:00:57 +0000

    added code to find and replace text

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -5,11 +5,22 @@
 import requests
 import json
 from config import key as cfg
+from git import Repo
 
 # get key from config file and url for repository
 apikey = cfg["wsaakey"]
 url = "https://api.github.com/repos/rayne-fall/wsaa-private"
 # get contents of repo and provide authorisation
 response = requests.get(url, auth = ('token', apikey))
-# sanity check
-print(response.status_code)
+# read in file
+with open ('text.txt', 'r') as text:
+  data=text.read()
+# replace Andrew with Sylvia
+data=data.replace("Andrew", "Sylvia")
+# write changes to file
+with open ('text.txt', 'w') as text:
+  text.write(data)
+
+
+
+# 1. https://stackoverflow.com/questions/17140886/how-to-search-and-replace-text-in-a-file
